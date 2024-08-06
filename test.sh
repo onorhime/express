7ea508b2820f925fd478837916a3eec9533a8b6c
Victory <onorhimevictory2017@gmail.com>
2024-08-06 06:51:53 +0100

    new

--- test.sh
+++ test.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
-echo "-----------------------------------------------------------------------------------------------"
-echo " 💣This command will remove your Migration PHP files , drop the database, and recreate it"
-echo "-----------------------------------------------------------------------------------------------"
-read -r -p "Are you sure you want to do this? [y/N] " response
-echo ""
-if [[ "$response" =~ ^[Yy]$ ]]
-then
-echo "rebuilding database ..."
-php bin/console doctrine:schema:drop -n -q --force --full-database
-rm migrations/*.php
-php bin/console make:migration
-php bin/console doctrine:migrations:migrate -n -q
-# php bin/console doctrine:fixtures:load
-fi
+
+# Define Symfony console command paths
+CONSOLE_PATH="bin/console"
+DOCTRINE_MIGRATIONS_PATH="vendor/bin/doctrine-migrations"
+
+# Remove previous migration files from the workspace
+rm -rf ./src/Migrations/*
+
+# Rebuild the database (drop, create schema, and load fixtures if needed)
+php $CONSOLE_PATH doctrine:database:drop --force
+php $CONSOLE_PATH doctrine:database:create
+php $CONSOLE_PATH doctrine:schema:update --force
+# Uncomment the line below if you want to load fixtures after rebuilding the schema
+# php $CONSOLE_PATH doctrine:fixtures:load --no-interaction
+
+# Generate a new migration
+php $DOCTRINE_MIGRATIONS_PATH generate
+
+# Run Doctrine migrations
+php $DOCTRINE_MIGRATIONS_PATH migrate
+
+# Remove previous migrations and their records from the database
+php $CONSOLE_PATH doctrine:migrations:version --delete --all
+
+echo "Remove previous migration files, rebuild, create migration, migrate, and delete previous migrations and migrated database completed successfully."
